4b1a1ebda92a1c594de8d9929435c0f8499f70c5
Zabi-Habibi <122958805+Zabi-Habibi@users.noreply.github.com>
2023-03-22 10:52:34 -0400

    added the necessary changes

---
+++ src/main/java/com/example/w23comp1008lhvideogame/Sprite.java
@@ -0,0 +1,75 @@
+package com.example.w23comp1008lhvideogame;
+
+import javafx.scene.image.Image;
+
+public class Sprite {
+    private Image image;
+    private int posX, posY, imageWidth, imageHeight, speed;
+    private boolean alive;
+
+    public Sprite(Image image, int posX, int posY, int imageWidth, int imageHeight, int speed) {
+        setImage(image);
+        setPosX(posX);
+        setPosY(posY);
+        setImageWidth(imageWidth);
+        setImageHeight(imageHeight);
+        setSpeed(speed);
+        alive = true;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public int getImageWidth() {
+        return imageWidth;
+    }
+
+    public void setImageWidth(int imageWidth) {
+        this.imageWidth = imageWidth;
+    }
+
+    public int getImageHeight() {
+        return imageHeight;
+    }
+
+    public void setImageHeight(int imageHeight) {
+        this.imageHeight = imageHeight;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+}
